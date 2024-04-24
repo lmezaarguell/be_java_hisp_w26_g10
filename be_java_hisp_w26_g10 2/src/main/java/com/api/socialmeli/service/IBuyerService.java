5079a3ffe815b9f522b7edd8acd118c0d5b41e0d
josemiguzman_meli <josemiguel.guzman@mercadolibre.com.co>
2024-04-24 16:43:20 -0500

    The BuyerFollowedListDTO method is created in the interface and the signature of the method is implemented in the class.

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/IBuyerService.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/IBuyerService.java
@@ -1,4 +1,7 @@
 package com.api.socialmeli.service;
 
+import com.api.socialmeli.dto.BuyerFollowedListDTO;
+
 public interface IBuyerService {
+    public BuyerFollowedListDTO getFolloedListByUser(Integer user_id);
 }
--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/BuyerServiceImpl.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/BuyerServiceImpl.java
@@ -1,8 +1,14 @@
 package com.api.socialmeli.service.impl;
 
+import com.api.socialmeli.dto.BuyerFollowedListDTO;
 import com.api.socialmeli.service.IBuyerService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BuyerServiceImpl implements IBuyerService {
+
+    @Override
+    public BuyerFollowedListDTO getFolloedListByUser(Integer user_id) {
+        return null;
+    }
 }
