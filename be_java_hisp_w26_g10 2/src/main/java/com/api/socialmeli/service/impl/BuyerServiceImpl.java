07b8d0eb122694c37dbc9335f9f5623ee0408f0e
josemiguzman_meli <josemiguel.guzman@mercadolibre.com.co>
2024-04-24 16:49:47 -0500

    The use case logic is implemented within the BuyerFollowedListDTO method.

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/BuyerServiceImpl.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/BuyerServiceImpl.java
@@ -1,14 +1,28 @@
 package com.api.socialmeli.service.impl;
 
 import com.api.socialmeli.dto.BuyerFollowedListDTO;
+import com.api.socialmeli.entity.Buyer;
+import com.api.socialmeli.exception.NotFoundException;
+import com.api.socialmeli.repository.IBuyerRepository;
 import com.api.socialmeli.service.IBuyerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BuyerServiceImpl implements IBuyerService {
 
+    @Autowired
+    IBuyerRepository buyerRepository;
+
     @Override
     public BuyerFollowedListDTO getFolloedListByUser(Integer user_id) {
-        return null;
+        ObjectMapper mapper = new ObjectMapper();
+        Buyer buyer = buyerRepository.getById(user_id);
+        if (buyer!=null){
+            return mapper.convertValue(buyer,BuyerFollowedListDTO.class);
+        }else {
+            throw new NotFoundException("El usuario no existe o no se encuentra registrado.");
+        }
     }
 }
