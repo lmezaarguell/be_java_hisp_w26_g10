3cbc5ed57f7aa9bdb32c17df749051484a4b9f92
josemiguzman_meli <josemiguel.guzman@mercadolibre.com.co>
2024-04-24 16:56:15 -0500

    The GetMapping method is implemented inside the controller for the use case.

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/controller/SocialMeliController.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/controller/SocialMeliController.java
@@ -1,8 +1,23 @@
 package com.api.socialmeli.controller;
 
+import com.api.socialmeli.repository.IBuyerRepository;
+import com.api.socialmeli.service.IBuyerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SocialMeliController {
+
+    @Autowired
+    IBuyerService buyerService;
+
+    @GetMapping("/users/{userId}/followed/list")
+    public ResponseEntity<?> getFollowedListById(@PathVariable Integer userId){
+        return ResponseEntity.status(HttpStatus.OK).body(buyerService.getFolloedListByUser(userId));
+    }
 
 }
