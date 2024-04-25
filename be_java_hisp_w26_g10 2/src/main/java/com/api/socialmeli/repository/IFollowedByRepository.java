f26098b62d23a963b098368972d29f25c3a5f37d
juagonzalezf_meli <juanangel.jgonzalezflores@mercadolibre.com.mx>
2024-04-25 09:02:11 -0600

    se carga funcionalidad al servicio de FollowedByService/addFollow

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/repository/IFollowedByRepository.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/repository/IFollowedByRepository.java
@@ -9,5 +9,6 @@ public interface IFollowedByRepository {
     void save(FollowedBy followedBy);
     List<FollowedBy> getFollowedByList(int seller_id);
     List<FollowedBy> getFollowsOfBuyer(int buyer_id);
+    Optional<FollowedBy> getFollowById(int buyer_id, int seller_id);
     void delete(int seller_id, int buyer_id);
 }
