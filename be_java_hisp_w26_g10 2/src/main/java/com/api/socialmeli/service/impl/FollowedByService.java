0bc6ef0a0437642542cf9b98d6f9984837d28254
juagonzalezf_meli <juanangel.jgonzalezflores@mercadolibre.com.mx>
2024-04-25 09:23:50 -0600

    se agrega funcionalidad al servicio de getFollowersOfSeller

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/FollowedByService.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/FollowedByService.java
@@ -1,20 +1,31 @@
 package com.api.socialmeli.service.impl;
 
+import com.api.socialmeli.dto.FollowedByDto;
 import com.api.socialmeli.dto.FollowedBySellerDto;
+import com.api.socialmeli.dto.UserDto;
 import com.api.socialmeli.entity.FollowedBy;
+import com.api.socialmeli.entity.Seller;
 import com.api.socialmeli.exception.BadRequestException;
+import com.api.socialmeli.exception.NotFoundException;
 import com.api.socialmeli.repository.IFollowedByRepository;
+import com.api.socialmeli.service.IBuyerService;
 import com.api.socialmeli.service.IFollowedByService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class FollowedByService implements IFollowedByService {
+    final static ObjectMapper objectMapper = new ObjectMapper();
+
 
     @Autowired
     IFollowedByRepository iFollowedByRepository;
+    IBuyerService iBuyerService;
 
     @Override
     public void addFollow(int seller_id, int buyer_id) {
@@ -30,13 +41,25 @@ public class FollowedByService implements IFollowedByService {
     }
 
     @Override
-    public FollowedBySellerDto getFollowersOfSeller(int seller_id) {
-        /*  */
-        return null;
+    public List<FollowedByDto> getFollowersOfSeller(Seller seller) {
+        /* se extrae la lista de seguidores del vendedor */
+        List<FollowedBy> followers = iFollowedByRepository.getFollowedByList(seller.getUser_id());
+
+        /* se comprueba si existen seguidores */
+        if(followers.isEmpty()){
+            throw new NotFoundException("El Vendedor no tiene seguidores");
+        }
+
+        // Mapear la lista de FollowedBy a FollowedByDto
+        List<FollowedByDto> followedByDtoList = followers.stream()
+                .map(followedBy -> objectMapper.convertValue(followedBy, FollowedByDto.class))
+                .collect(Collectors.toList());
+
+        /* se retorna la respuesta */
+        return followedByDtoList;
     }
 
     @Override
     public void deleteFollow(int seller_id, int buyer_id) {
-
     }
 }
