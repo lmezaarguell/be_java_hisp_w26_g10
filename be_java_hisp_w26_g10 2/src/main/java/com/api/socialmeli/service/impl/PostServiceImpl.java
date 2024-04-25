1c0e017111ba8e8c351b0122d034453db34e8c7c
John Stiven Garcia Vargas <johnstiven.garciavargas@mercadolibre.com.co>
2024-04-25 10:58:04 -0500

    The implementation was commented on and improved

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/PostServiceImpl.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/PostServiceImpl.java
@@ -31,56 +31,71 @@ public class PostServiceImpl implements IPostService {
 
     @Override
     public PostsByFollowedDto getPostsByFollowed(Integer userId, String order) {
+        //Se busca el comprador y se guarda en una instacia de buyer con el id del usuario recibido
         Buyer buyer = buyerService.getBuyerById(userId);
 
         if (buyer == null) {
+            //Si no se encontro el comprador lanza la excepcion Not Found Exception
             throw new NotFoundException("No se encontro el usuario con id: " + userId);
         }
 
+        //Se crea la instancia del DTO que se va a devolver al controlador y se coloca el id del comprador en el dto
         PostsByFollowedDto postByFollowedDto = new PostsByFollowedDto();
         postByFollowedDto.setUser_id(userId);
 
+        //Se obtine la lista de ids de los compradores que sigue el comprador
         List<Integer> ids = buyer.getFollowed().stream().map(Seller::getUser_id).toList();
         if (ids.isEmpty()) {
+            //Si no esta siguiendo a nadie devuelve en dto al controlador
             postByFollowedDto.setPosts(new ArrayList<>());
             return postByFollowedDto;
         }
+
+        //Se crean las fechas minima y maxima para comprender el periodo de los ultimos 15 dias
         LocalDate minDate = LocalDate.now().minusDays(15);
         LocalDate maxDate = LocalDate.now().plusDays(1);
 
-        List<Post> postsFollowed;
-        if(order.equalsIgnoreCase("date_desc")) {
-            postsFollowed = postRepository.getAll().stream().
-                    filter(post -> findIdInPost(post.getUser_id(), ids) && post.getDate().isAfter(minDate)
-                            && post.getDate().isBefore(maxDate)).sorted(Comparator.comparing(Post::getDate).reversed()).toList();
-        }else if(order.equalsIgnoreCase("date_asc")) {
-            postsFollowed = postRepository.getAll().stream().
-                    filter(post -> findIdInPost(post.getUser_id(), ids) && post.getDate().isAfter(minDate)
-                            && post.getDate().isBefore(maxDate)).sorted(Comparator.comparing(Post::getDate)).toList();
-        }else{
-           throw new BadRequestException("El orden pedido no es valido");
-        }
+        /*Se obtien todos las publicaciones, se filtra por las que fueron creadas por los vendedores que sigue
+        con ayuda de la funcion finIdInPost y tambien se filtra con con las fecha minDate y maxDate para obtener
+        las de los ultimos quince dias
+         */
+        List<Post> postsFollowed= postRepository.getAll().stream().
+                filter(post -> findIdInPost(post.getUser_id(), ids) && post.getDate().isAfter(minDate)
+                        && post.getDate().isBefore(maxDate)).toList();
 
         if (postsFollowed.isEmpty()) {
+            //Si los vendedores que sigue no tiene publicaciones devuelve el dto al controlador
             postByFollowedDto.setPosts(new ArrayList<>());
             return postByFollowedDto;
         }
 
-        List<PostDto> posts = new ArrayList<>();
-        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        for (Post post : postsFollowed) {
-            System.out.println(post);
-            PostDto postDto = mapper.convertValue(post, PostDto.class);
-            posts.add(postDto);
-        }
+        //Se llama a la funcion para ordenar las publicaciones segun los el parametro de order
+        postsFollowed = orderPostByDate(postsFollowed,order);
 
-        postByFollowedDto.setPosts(posts);
+        //Se crea el mapper para transformar las publicaciones a dto
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        //Se colocan las lista de las publicaciones transformadas en el dto
+        postByFollowedDto.setPosts(
+                postsFollowed.stream().map(post -> mapper.convertValue(post, PostDto.class)).toList());
 
         return postByFollowedDto;
     }
 
-
+     /*Determina si el post es del vendedor que esta siguiendo, con la lista de ids de los vendedores que sigue
+       y el id del creador de la publicacion*/
     private boolean findIdInPost(Integer idUser, List<Integer> ids) {
         return ids.stream().anyMatch(id -> id.equals(idUser));
+    }
+
+    //Ordena la lista de post por fecha segun el orden enviado por parametro
+    List<Post> orderPostByDate(List<Post> posts, String order) {
+        if(order == null || order.equalsIgnoreCase("date_asc")) {
+             return posts.stream().sorted(Comparator.comparing(Post::getDate)).toList();
+        }else if(order.equalsIgnoreCase("date_desc")) {
+            return posts.stream().sorted(Comparator.comparing(Post::getDate).reversed()).toList();
+        }else {
+            throw new BadRequestException("El orden pedido no es valido");
+        }
     }
 }
