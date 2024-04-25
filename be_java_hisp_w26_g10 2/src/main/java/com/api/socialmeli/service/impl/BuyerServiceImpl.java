fe1ee7ca3b3297655567fb2dde913489f7874e96
josemiguzman_meli <josemiguel.guzman@mercadolibre.com.co>
2024-04-25 13:13:04 -0500

    pull request main to US2

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/BuyerServiceImpl.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/service/impl/BuyerServiceImpl.java
@@ -14,8 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 public class BuyerServiceImpl implements IBuyerService {
 
