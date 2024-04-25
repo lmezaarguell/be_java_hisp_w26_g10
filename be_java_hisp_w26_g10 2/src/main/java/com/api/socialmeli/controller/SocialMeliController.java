f94be598d7f12146cf27ebbd3529122673e233ef
John Stiven Garcia Vargas <johnstiven.garciavargas@mercadolibre.com.co>
2024-04-24 19:13:30 -0500

    Add endpoint to get posts by followed

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/controller/SocialMeliController.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/controller/SocialMeliController.java
@@ -1,8 +1,23 @@
 package com.api.socialmeli.controller;
 
+import com.api.socialmeli.service.IPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/products")
 public class SocialMeliController {
+
+    @Autowired
+    private IPostService postService;
+
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<?> getPostsByFollowed(@PathVariable Integer userId) {
+        return ResponseEntity.ok().body(postService.getPostsByFollowed(userId));
+    }
 
 }
