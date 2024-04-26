fee74540d688252acf6350b96136868ce74676c4
Mateo0211 <caldera.m13@hotmail.com>
2024-04-25 19:17:04 -0500

    Solving post repository error

--- be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/repository/impl/PostRepositoryImpl.java
+++ be_java_hisp_w26_g10 2/src/main/java/com/api/socialmeli/repository/impl/PostRepositoryImpl.java
@@ -1,21 +1,16 @@
 package com.api.socialmeli.repository.impl;
 
 import com.api.socialmeli.entity.Post;
-import com.api.socialmeli.entity.Seller;
 import com.api.socialmeli.repository.IPostRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.ResourceUtils;
 
-import javax.imageio.IIOException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public class PostRepositoryImpl implements IPostRepository {
@@ -23,11 +18,6 @@ public class PostRepositoryImpl implements IPostRepository {
 
     public PostRepositoryImpl(){
         this.post = this.loadData();
-    }
-
-    @Override
-    public Post save() {
-        return null;
     }
 
     @Override
@@ -74,5 +64,10 @@ public class PostRepositoryImpl implements IPostRepository {
     public int searchPostId(){
         int max = this.post.stream().mapToInt(Post::getPost_id).max().orElse(0);
         return max + 1;
+    }
+
+    @Override
+    public void savePost(Post post){
+        this.post.add(post);
     }
 }
